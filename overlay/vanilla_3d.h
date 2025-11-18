d96caf5182d2a5a5ac53f211894ca01b2205fefd
sophtsang <sophiatsang19@gmail.com>
2025-11-18 23:39:18 +0000

    modifications to icp in overlay

---
+++ overlay/vanilla_3d.h
@@ -0,0 +1,38 @@
+/**
+ * @copyright Copyright (C) 2025 Cornell Electric Vehicles.
+ * SPDX-License-Identifier: MIT
+ */
+#pragma once
+
+#include <memory>
+#include <Eigen/Dense>
+#include "icp/icp.h"
+#include "icp/config.h"
+#include "algo/kdtree.h"
+
+namespace icp {
+
+    class Vanilla3d final : public ICP3 {
+    public:
+        Vanilla3d(const Config& config);
+        Vanilla3d();
+        ~Vanilla3d() override;
+
+    protected:
+        void setup() override;
+        void iterate() override;
+        double calculate_cost() const override;
+
+    private:
+        PointCloud c;
+        std::unique_ptr<KdTree<Vector>> target_kdtree_;
+        double current_cost_;
+        std::unique_ptr<icp::KdTree<Eigen::Vector3d>> kdtree_;
+
+        Neighbors nearest_neighbor(const PointCloud& src);
+        static double dist(const Eigen::Vector3d& pta, const Eigen::Vector3d& ptb);
+        static RBTransform best_fit_transform(const PointCloud& A, const PointCloud& B);
+        void calculate_cost(const std::vector<double>& distances);
+    };
+
+}  // namespace icp
